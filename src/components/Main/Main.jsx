20ec891e69593db4cb9f466e06cf18f84e9162fc
Rinku Kumar Maurya <rinkukumarmaurya221@gmail.com>
2021-12-19 21:51:12 +0530

    create basic app structure

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { Card, CardContent, CardHeader, Grid, Typography, Divider } from '@material-ui/core'
+
+import useStyles from './styles'
+
+const Main = () => {
+    const classes = useStyles();
+    
+    return (
+        <Card className={classes.root}>
+            <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+            <CardContent>
+                <Typography variant='h5' align='center'>Total Balance $1000</Typography>
+                <Typography variant='subtitle1' style={{lineHeight: '1.5em', marginTop: '20px'}}>
+                    {/* Info Card */}
+                    Try saying: Add income for $20 in category Salary from next Sunday.
+                </Typography>
+                <Divider />
+                {/* Form */}
+            </CardContent>
+            <CardContent className={classes.cardContent}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        {/* List */}
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Main
